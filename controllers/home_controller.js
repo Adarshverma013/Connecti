c15d4ca38980084fbc7df205719b68e525dd3938
adarshverma013 <adarshverma013@gmail.com>
2021-02-01 21:29:03 +0530

    Added first controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+//* controller for home page
+module.exports.home = function (req,res) {
+    return res.end('<h1>Express is up and running....</h1>');
+}
+
+//  module.exports.actioName = function(req,res){}
