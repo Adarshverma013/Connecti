09d86d5eaec2adefc6cc2f49ca4da48287e7b883
adarshverma013 <adarshverma013@gmail.com>
2021-02-01 23:53:09 +0530

    Set view engine to EJS

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 //* use express router
 app.use('/',require('./routes'));
 
+//* set view engine 
+app.set('view engine','ejs');
+
+//* locate views folder
+app.set('views','./views');
 
 // start server
 app.listen(port,function (err) {
