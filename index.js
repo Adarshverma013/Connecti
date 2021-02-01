7fedb45cc84aca73d0b9e34fd56815bd075e5bfd
adarshverma013 <adarshverma013@gmail.com>
2021-02-01 21:13:34 +0530

    Set up router

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 8000;
 
 
+//* use express router
+app.use('/',require('./routes'));
+
+
+// start server
 app.listen(port,function (err) {
    if(err){
        console.log(`Error while starting server: ${err}`);
