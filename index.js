d9a80a2ad0549d445a9ef4d8da1b7a89e36f3338
adarshverma013 <adarshverma013@gmail.com>
2021-02-03 22:48:41 +0530

    Database set

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose'); //* locating  database
 
 
+//* set up static files
 app.use(express.static('./assets'));
 
 //* setting up layouts
 app.use(expressLayouts);
+
+//* extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 //* use express router
 app.use('/',require('./routes'));
