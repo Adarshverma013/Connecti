8d768749d73cc2b64f4bf3866ff492ab5ab5a1d6
adarshverma013 <adarshverma013@gmail.com>
2021-02-03 23:39:58 +0530

    Sign up Sign In Form Created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,29 @@
+//* render user profile
 module.exports.profile = function (req,res) { 
     return res.render('user_profile',{
         title: 'User Profile'
     });
  }
+
+//* render sign in page
+module.exports.signUp = function (req,res) { 
+     return res.render('user_sign_up',{
+         title: "ConnectI | Sign Up"
+     });
+  }
+//* render sign up page
+module.exports.signIn = function (req,res) { 
+    return res.render('user_sign_in',{
+        title: "ConnectI | Sign In"
+    });
+ }
+ 
+//* get the sign up data
+module.exports.create = function (req,res) { 
+    //TODO later
+ }
+
+module.exports.createSession = function (res, req) { 
+    //TODO later
+ }
+ 
