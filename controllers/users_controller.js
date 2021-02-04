53caadf39dcd2c96bf671d46be84af0e7921fde8
adarshverma013 <adarshverma013@gmail.com>
2021-02-04 19:29:40 +0530

    Sign up controller created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 //* render user profile
 module.exports.profile = function (req,res) { 
     return res.render('user_profile',{
@@ -20,10 +22,25 @@ module.exports.signIn = function (req,res) {
  
 //* get the sign up data
 module.exports.create = function (req,res) { 
-    //TODO later
- }
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function (err, user) {
+       if(err){console.log('error in finding user in signing up'); return;}
+       
+       if(!user){
+           User.create(req.body, function(err,user){
+                if(err){console.log('error in creating user while signing up'); return;}
+
+                return res.redirect('/users/sign-in');
+           });
+       }else{
+           return res.redirect('back');
+       }
+    });
+}
 
 module.exports.createSession = function (res, req) { 
     //TODO later
- }
+}
  
