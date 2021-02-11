d87f33490705cc71cb770428c7ad07e4dd384810
adarshverma013 <adarshverma013@gmail.com>
2021-02-12 01:04:01 +0530

    post model created and create post functionality implemented

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,20 @@
+const Post = require('../models/post');
 //* controller for home page
 module.exports.home = function (req,res) {
-    return res.render('home',{
-        title: 'Home'
-    });
+    // Post.find({},function (err,post) { 
+
+    //     return res.render('home',{
+    //         title: 'Home',
+    //         posts: post
+    //     });
+    // });
+    //* populate the user of each post
+    Post.find({}).populate('user').exec(function (err,post) { 
+        return res.render('home',{
+            title: 'Home',
+            posts: post
+        });
+     });
 }
 
 //  module.exports.actioName = function(req,res){}
